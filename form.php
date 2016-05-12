66495a5dcaa4454ad4031a3bdd6d59dbd99bb04b
nnasser@outlook.com <naalnas@microsoft.com>
2016-05-12 13:33:44 +0300

    First Commit

---
+++ form.php
@@ -0,0 +1,9 @@
+<div align=center> 
+
+<form action="result.php" method="post">
+     <img alt="Random Number" src="image.php"> <br/>
+     <input type="text" name="num"><br />
+     <input type="submit" name="submit" value="Check">
+</form>
+
+</div>
---
+++ result.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if($_SESSION['img_number'] != $_POST['num'])
+{
+    echo "<div align=center> ";
+    echo " <font color = red> <h2> The number you entered doesnt match the image. </h2> </font> <br> <a href=form.php>Try Again</a><br>";
+    echo "</div> ";
+}else{
+     echo "<div align=center> ";
+    echo'<font color = green> <h2> The numbers Match! </h2> </font><br>
+    <a href="form.php">Try Again</a><br>';
+    echo "</div> ";
+}
+?>
