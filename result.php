3b8a420044da70e8e5f0ab0e0c9ad37ba1fb7dc8
nnasser@outlook.com <naalnas@microsoft.com>
2016-05-17 17:07:45 +0300

    Increase font size

--- result.php
+++ result.php
@@ -3,11 +3,11 @@ session_start();
 if($_SESSION['img_number'] != $_POST['num'])
 {
     echo "<div align=center> ";
-    echo " <font color = red> <h2> The number you entered doesnt match the image. </h2> </font> <br> <a href=form.php>Try Again</a><br>";
+    echo " <font color = red> <h1> The number you entered doesnt match the image. </h1> </font> <br> <a href=form.php>Try Again</a><br>";
     echo "</div> "; 
 }else{
      echo "<div align=center> ";
-    echo'<font color = green> <h2> The numbers Match! </h2> </font><br>
+    echo'<font color = green> <h1> The numbers Match! </h1> </font><br>
     <a href="index.php">Try Again</a><br>';
     echo "</div> ";
 }
