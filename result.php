9812077587f102a249664c3727f46666960c3f39
nnasser@outlook.com <naalnas@microsoft.com>
2016-05-19 14:05:32 +0300

    Change font size to H1

--- result.php
+++ result.php
@@ -3,11 +3,11 @@ session_start();
 if($_SESSION['img_number'] != $_POST['num'])
 {
     echo "<div align=center> ";
-    echo " <font color = red> <h3> The number you entered doesnt match the image. </h3> </font> <br> <a href=form.php>Try Again</a><br>";
+    echo " <font color = red> <h1> The number you entered doesnt match the image. </h1> </font> <br> <a href=form.php>Try Again</a><br>";
     echo "</div> "; 
 }else{
      echo "<div align=center> ";
-    echo'<font color = green> <h3> The numbers Match! </h3> </font><br>
+    echo'<font color = green> <h1> The numbers Match! </h1> </font><br>
     <a href="index.php">Try Again</a><br>';
     echo "</div> ";
 }
